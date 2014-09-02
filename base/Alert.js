ea7c5a516e7b94386b6458db9dc0a2bd18fcbfe9
Roman Levchenko <larexsetch@gmail.com>
2014-09-02 16:13:00 +0700

    Update

--- base/Alert.js
+++ base/Alert.js
@@ -10,7 +10,11 @@ var Alert = function (options) {
     this.closeButton = null;
     this.container = null;
     this.options = {
-        'title': 'System message:&nbsp;'
+        'title': 'System message:&nbsp;',
+        'bootstrap': false,
+        'autoAppend': true,
+        'withTitle': true,
+        'type': ''
     };
     this.body = document.getElementsByTagName('body')[0];
 
@@ -18,7 +22,50 @@ var Alert = function (options) {
         this.container = document.createElement('div');
         this.createCloseButton();
         this.container.appendChild(this.closeButton);
-        this.container.className = 'alert';
+        this.container.className = 'alert' + (this.options.type == '' ? '' : ' alert-' + this.options.type);
+        if (this.options.bootstrap == false) {
+            this.setStyleContainer();
+        }
+        if (this.options.autoAppend) {
+            this.body.appendChild(this.container);
+        }
+    };
+
+    this.createCloseButton = function () {
+        var self = this;
+        this.closeButton = document.createElement('a');
+        this.closeButton.innerHTML = '&times;';
+        this.closeButton.className = 'close';
+        this.closeButton.setAttribute('data-dismiss', 'alert');
+        if (this.options.bootstrap == false) {
+            this.setStyleCloseButton();
+        }
+        this.closeButton.onclick = function () {
+            self.close();
+        }
+    };
+    this.createContent = function () {
+        if (this.options.withTitle) {
+            var title = document.createElement('h4');
+            title.className = 'title';
+            title.innerHTML = this.options.title;
+            if (this.options.bootstrap == false) {
+                this.setStyleTitle(title);
+            }
+            this.container.appendChild(title);
+        }
+        var content = document.createElement('div');
+        content.className = 'content';
+        content.innerHTML = this.options.content;
+        if (this.options.bootstrap == false) {
+            this.setStyleContent(content);
+        }
+        this.container.appendChild(content);
+    };
+    this.close = function () {
+        this.body.removeChild(this.container);
+    };
+    this.setStyleContainer = function () {
         this.container.css({
             'position': 'fixed',
             'min-height': '30px',
@@ -28,14 +75,8 @@ var Alert = function (options) {
             'z-index': '1000000',
             'background': '#FDD'
         });
-        this.body.appendChild(this.container);
     };
-
-    this.createCloseButton = function () {
-        var self = this;
-        this.closeButton = document.createElement('a');
-        this.closeButton.innerHTML = '&times;';
-        this.closeButton.className = 'close';
+    this.setStyleCloseButton = function () {
         this.closeButton.css({
             'position': 'absolute',
             'font-size': '18px',
@@ -44,43 +85,32 @@ var Alert = function (options) {
             'right': '10px',
             'cursor': 'pointer'
         });
-        this.closeButton.onclick = function () {
-            self.close();
-        }
     };
-    this.createContent = function () {
-        var title = document.createElement('h4');
-        var content = document.createElement('div');
-        title.className = 'title';
-        title.innerHTML = this.options.title;
+    this.setStyleTitle = function (title) {
         title.css({
             'display': 'inline-block',
             'line-height': '12px',
             'font-size': '12px',
             'margin': '10px 0px 10px 20px'
         });
-        content.className = 'content';
-        content.innerHTML = this.options.content;
+    };
+    this.setStyleContent = function (content) {
         content.css({
             'display': 'inline-block',
             'line-height': '12px',
             'font-size': '12px'
         });
-        this.container.appendChild(title);
-        this.container.appendChild(content);
-    };
-    this.close = function () {
-        this.body.removeChild(this.container);
     };
     if (typeof options == 'string') {
         this.options['content'] = options;
     } else {
-        for(var key in options) {
+        for (var key in options) {
             this.options[key] = options[key];
         }
     }
     this.createAlert();
     this.createContent();
+    return this;
 };
 window.alert = function (message) {
     return new Alert(message)
